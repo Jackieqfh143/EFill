ce6e69614e53bc72b2a301d6d89105930eb307d8
Codeoops <528960108@qq.com>
2023-04-09 13:52:49 +0800

    Update visualize.py

--- utils/visualize.py
+++ utils/visualize.py
@@ -2,6 +2,7 @@ import glob
 import os
 from .html import HTML
 import random
+from .util import get_file_info
 
 
 
@@ -16,10 +17,12 @@ def show_im2_html(web_title,web_header,web_dir,img_dir,im_size=256,max_num = 200
 
     selected_imgs_idx = [random.randint(0,len(imgs) // 3 - 1) for i in range(max_num)]
 
+    img_suffex = get_file_info(imgs[1])["file_type"]
+
     for idx in selected_imgs_idx:
-        masked_name = f'{idx:0>5d}_im_masked.png'
-        comp_name = f'{idx:0>5d}_im_out.png'
-        gt_name = f'{idx:0>5d}_im_truth.png'
+        masked_name = f'{idx:0>5d}_im_masked{img_suffex}'
+        comp_name = f'{idx:0>5d}_im_out{img_suffex}'
+        gt_name = f'{idx:0>5d}_im_truth{img_suffex}'
         txts = [masked_name,comp_name,gt_name]
         ims = []
         links = []
@@ -33,11 +36,4 @@ def show_im2_html(web_title,web_header,web_dir,img_dir,im_size=256,max_num = 200
     webpage.save()
 
 
-if __name__ == '__main__':
-    show_im2_html(web_title=f"Result_place",
-                  web_header=f"Inpainting Results on place",
-                  web_dir="./",
-                  img_dir="/home/codeoops/CV/Remote4/results/Place/thick_256",
-                  im_size=256,
-                  max_num=200)
 
